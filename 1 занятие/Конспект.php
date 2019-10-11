760a69b8c42351f836e137e9192faf80e399fd03
Akramion <leha.bushuev@gmail.com>
2019-10-11 19:21:29 +0500

    init

---
+++ 1 занятие/Конспект.php
@@ -0,0 +1,58 @@
+<?php 
+
+$numberFirst = 5; // Integer 
+
+$float = 4.5; // Float 
+$string = "string"; // string
+$boolean = false; // boolean  true/false
+
+
+$numberFirst = 2 ** 0;  // +, -, /, *, %, **
+$boolean = 3; // >, <, ==, ===, =>, <=, !, <=>
+
+// Условные конструкции
+
+// if($numberFirst > 3) {
+// 	echo "Число больше 3";
+// }
+
+// else if($numberFirst == 1) {
+// 	echo "Число равно 1";
+// }
+
+// else {
+// 	echo "Неправильное число";
+// }
+
+// ++ - инкремент, -- - декремент
+
+//Цикл while
+
+// $i = 0;
+// while($i < 4) {
+// 	$i++;
+
+// 	if($i == 2) {
+// 		break; // continue
+// 	}
+
+// 	echo "Поехали";
+// }
+
+
+// Do while
+
+// do {
+
+// echo "string";
+
+// } while(false)
+
+//Цикл for
+
+for($i = 0; $i < 5; $i++) {
+	echo "Привет <br>";
+}
+
+?>
+
